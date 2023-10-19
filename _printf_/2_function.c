06daeb2b8c90b7c6d7b340574087272a6442b9dc
Prof-Oj01 <ojoofe20@gmail.com>
2023-10-18 22:52:48 -0700

    i don tire

--- _printf_/2_function.c
+++ _printf_/2_function.c
@@ -1,47 +1,45 @@
 #include "main.h"
-/**
-  * print_b - prints a binary
-  * @binry: variable placeholder
-  * Return: 0
-  */
 
+/**
+ * print_b - prints a binary number
+ * @binry: va_list argument containing the number to print
+ * Return: number of characters printed
+ */
 int print_b(va_list binry)
 {
-	unsigned int num = va_arg(binry, unsigned int);
-	int chrts_count = 0;
-	int k, i;
-	int initial = 0;
+    unsigned int num = va_arg(binry, unsigned int);
+    int chrts_count = 0;
+    int k;
+    int initial = 0;
+    char binary[32];
 
-	char binary[32];
+    for (k = 31; k >= 0; k--)
+    {
+        if ((num >> k) & 1)
+        {
+            initial = 1;
+        }
 
-	for (k = 31; k >= 0; k--)
-	{
-		if ((num >> k) & 1)
-		{
-			initial = 1;
-		}
+        if (initial)
+        {
+            binary[31 - k] = ((num >> k) & 1) ? '1' : '0';
+        }
+    }
 
-		if(initial)
-		{
-			binary[31 - k] = ((num >> k) & 1) ? '1' : '0';
-		}
-	}
+    binary[32] = '\0';
 
-	binary[32] = '\0';
+    if (initial == 0)
+    {
+        our_putchar('0');
+        chrts_count++;
+    }
 
-	if(initial)
-	{
-		for (i = 0; i < 32; i++)
-		{
-			our_putchar(binary[i]);
-			chrts_count++;
-		}
-	}
-	else
-	{
-		our_putchar('0');
-		chrts_count++;
-	}
+    for (k = 0; k < 32; k++)
+    {
+        our_putchar(binary[k]);
+        chrts_count++;
+    }
 
-	return (chrts_count);
+    return chrts_count;
 }
+
--- _printf_/_printf.c
+++ _printf_/_printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _printf - function that produces output to the stdout formatted text
  *
