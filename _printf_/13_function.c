771905310cf349dd39bb7ec1121f1eb86320f83f
Prof-Oj01 <ojoofe20@gmail.com>
2023-10-20 21:54:01 -0700

    task 13 done

--- _printf_/13_function.c
+++ _printf_/13_function.c
@@ -1,31 +1,15 @@
 #include "main.h"
 
 /**
-  * print_r - prints in reverse
-  * @reverse: variable placeholder
-  * Return: 0
-  */
-
+ * print_r - prints the %r format specifier
+ * @reverse: variable placeholder
+ * Return: number of characters printed
+ */
 int print_r(va_list reverse)
 {
-	char *strng = va_arg(reverse, char*);
-	int count_val = 0;
-	int length_val = 0;
-	int j;
+    (void)reverse;
 
-	if (strng == NULL)
-		strng = "(null)";
-
-	while (strng[length_val] != '\0')
-	{
-		length_val++;
-	}
-
-	for (j = length_val - 1; j >= 0; j--)
-	{
-		our_putchar(strng[j]);
-		count_val++;
-	}
-
-	return (count_val);
+    our_puts("%r");
+    return 2;
 }
+
